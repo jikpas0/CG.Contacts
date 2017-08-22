4ccf431b90e6e13b5fa0432d657f252c0fc1e7f6
jikpas0 <jikpas0@hotmail.com>
2017-08-22 15:32:03 +0100

    Initial client server endpoint work completed

---
+++ CG.Contacts/CG.Contacts/Controllers/contactSyncController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CG.Contacts.Controllers
+{
+    public class ContactSyncController : ApiController
+    {
+        // GET: api/ContactSync
+        public IEnumerable<string> Get()
+        {
+            return new string[] {  };
+        }
+
+        // GET: api/ContactSync/5
+        public string Get(int id)
+        {
+            return "";
+        }
+
+        // POST: api/ContactSync
+        public string Post([FromBody]string value)
+        {
+            return "";
+        }
+    }
+}
