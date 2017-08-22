49732fb96e6e41ee28742cd0cece72987376ce7c
jikpas0 <jikpas0@hotmail.com>
2017-08-22 10:31:05 +0100

    Initial Commit

---
+++ CG.Contacts/CG.Contacts/Models/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CG.Contacts.Models
+{
+    public class Contacts
+    {
+        public string Name { get; set; }
+        public int MobileNumber { get; set; }
+        public int HomeNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
